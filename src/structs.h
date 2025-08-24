c705f7a20b4d8bb83e7df643747a51666d20dc13
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2025-08-24 13:52:40 +0300

    allow _FORTIFY_SOURCE=3
    Sorry VMS users

--- src/structs.h
+++ src/structs.h
@@ -1780,7 +1780,7 @@ struct dictitem_S
 {
     typval_T	di_tv;		// type and value of the variable
     char_u	di_flags;	// DI_FLAGS_ flags (only used for variable)
-    char_u	di_key[1];	// key (actually longer!)
+    char_u	di_key[];	// key (actually longer!)
 };
 typedef struct dictitem_S dictitem_T;
 
